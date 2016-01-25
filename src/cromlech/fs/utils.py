d00b7c8e116e3d01041b3e60f6b45d88cf64097b
Souheil CHELFOUH <trollfot@gmail.com>
2016-01-25 18:57:49 +0100

    Factored folder access checkings into a util method

--- src/cromlech/fs/utils.py
+++ src/cromlech/fs/utils.py
@@ -16,6 +16,20 @@ def access_permissions(path, *perms):
     return True
 
 
+def assert_directory_access(path, mkdir=False):
+    if not os.path.exists(path):
+        if mkdir is True:
+            os.makedirs(path, 0755)
+            return True
+        else:
+            raise FSError('Folder %r does not exist.' % path)
+    if not os.path.isdir(path):
+        raise FSError('%r exists and is not a folder' % path)
+    else:
+        assert access_permissions(realpath, *RXW)
+    return True
+
+
 def create_directory(path, chroot=None):
     """Returns True if the creation was complete or if
     the folder already exists and is writeable.
@@ -32,12 +46,4 @@ def create_directory(path, chroot=None):
                               (path, chroot))
         assert access_permissions(rooted, *RXW)
 
-    if os.path.exists(realpath):
-        if not os.path.isdir(realpath):
-            raise FSError('%r exists and is not a folder' % path)
-        else:
-            assert access_permissions(realpath, *RXW)
-    else:
-        os.makedirs(realpath, 0755)
-
-    return True
+    return assert_directory_access(realpath, mkdir=True)
